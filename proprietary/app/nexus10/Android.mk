3a19525be5f1f6752ce2bd43d2a1d5928e0eb25f
Paul Beeler <sparksco@gmail.com>
2013-05-21 01:55:02 -0600

    Add manta gallery and video editor Signed-off-by: Paul Beeler <sparksco@gmail.com>

---
+++ proprietary/app/nexus10/Android.mk
@@ -0,0 +1,22 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := GalleryGoogle-manta
+LOCAL_SRC_FILES := GalleryGoogle-manta.apk
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_PATH := $(TARGET_OUT)/app
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := VideoEditorGoogle-manta
+LOCAL_SRC_FILES := VideoEditorGoogle-manta.apk
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_PATH := $(TARGET_OUT)/app
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_PREBUILT)
+
